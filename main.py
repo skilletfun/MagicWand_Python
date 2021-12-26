c01a276c2af067fd0d91ef3debf1c0a207c7f372
skilletfun <60892021+skilletfun@users.noreply.github.com>
2021-12-26 11:54:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,79 +3,88 @@ import sys
 
 sys.setrecursionlimit(500000)
 
-start_point = [50, 50]
-img_array = []
-counter_array = []
-
-dopusk = 1
-
+#===============================================================#
+# Colors and init data
+#===============================================================#
 WHITE = [255,255,255]
 BLACK = [0,0,0]
 RED = (255,0,0)
 GREEN = (0,255,0)
+color = WHITE
 
+start_point = [2, 1]
+main_array = []
+contour_array = []
+
+dopusk = 1
+
+#===============================================================#
+# Open image and generate list with changable items
+#===============================================================#
 img = Image.open('sss.png')
 obj = img.load()
 
-color = WHITE
+# Fill the array with zeros
+img_array = []
+for _ in range(img.height):
+    temp = []
+    for _ in range(img.width):
+        temp.append([0,0,0])
+    img_array.append(temp)
+    
+# Fill the array with pixels    
+for i in range(img.height):
+    for j in range(img.width):
+        pix = obj[i,j]
+        img_array[i][j] = list(pix)
 
-main_arr = [list(obj[x,y]) for y in range(img.height) for x in range(img.width)]
-
-#print(main_arr)
-
+#===============================================================#
+# Check color
+#===============================================================#
 def check_color(pix):
     RGB_R = abs(pix[0] - color[0])
     RGB_G = abs(pix[1] - color[1])
     RGB_B = abs(pix[2] - color[2])
     
-#    print(str(RGB_R) + ' ' + str(RGB_G) + ' ' + str(RGB_B))
-    
     if RGB_R <= dopusk and RGB_G <= dopusk and RGB_B <= dopusk:
         return True
     else:
-#        print('FALSE')
         return False
 
-#print(len(main_arr))
-
-
 #===============================================================#
-def pixel(p_i, p_j):
-    return img.width * p_j + p_i
+# Check pixel color
 #===============================================================#
-
 def check(p_i, p_j):
-#    print('Start check '+str(p_i)+' '+str(p_j))
     if 0 <= p_i < img.width and 0 <= p_j < img.height:
-#        print('Passed borders')
-        if main_arr[pixel(p_i,p_j)][0] >= 0:
-#            print('Pixel '+str(p_i)+' '+str(p_j)+' more than 0')
-            if check_color(main_arr[pixel(p_i,p_j)]):
-#                print('Pixel '+str(p_i)+' '+str(p_j)+' has ACCEPTED color')
-                main_arr[pixel(p_i,p_j)] = (-1, -1, -1)
-                img_array.append([p_i, p_j])
-                
+        if img_array[p_j][p_i][0] >= 0:
+            if check_color(img_array[p_j][p_i]):
+                img_array[p_j][p_i] = [-1, -1, -1]
+                main_array.append([p_j, p_i])
                 check(p_i + 1, p_j)
                 check(p_i, p_j + 1)
                 check(p_i - 1, p_j)
                 check(p_i, p_j - 1)
             else:
-#                print('Pixel '+str(p_i)+' '+str(p_j)+' has DECLINE color')
-                obj_color = main_arr[pixel(p_i,p_j)]
-                main_arr[pixel(p_i,p_j)] = (-2, -2, -2)
-#                print('APPEND')
-                counter_array.append([[p_i, p_j], obj_color])
+                img_array[p_j][p_i] = [-2, -2, -2]
+                contour_array.append([p_j, p_i])
 
 #===============================================================#
 
-check(50, 50)
+check(50,50)
 
-for el in img_array:
+for el in main_array:
     obj[el[0], el[1]] = RED
     
-#print(counter_array)
-    
-for el in counter_array:
-    obj[el[0][0], el[0][1]] = GREEN
+for el in contour_array:
+    obj[el[0], el[1]] = GREEN
 
-img.show()
+contour_array.sort()
+
+print(contour_array)
+#img.show()
+img.close()
+
+
+
+
+
